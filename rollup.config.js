8ac0b13494de77c22b65f487af511c6fb2a0174f
Vince Au <vinceau09@gmail.com>
2020-05-16 15:58:33 +1000

    Add minification

--- rollup.config.js
+++ rollup.config.js
@@ -2,46 +2,57 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
 import sass from "rollup-plugin-sass";
 import copy from "rollup-plugin-copy";
 
-import packageJson from "./package.json";
+import pkg from "./package.json";
+
+const minifyExtension = (pathToFile) => pathToFile.replace(/\.js$/, ".min.js");
 
 export default {
   input: "src/index.ts",
   output: [
     {
-      file: packageJson.main,
+      file: pkg.main,
       format: "cjs",
-      sourcemap: true
+      sourcemap: true,
     },
     {
-      file: packageJson.module,
+      file: minifyExtension(pkg.main),
+      format: "cjs",
+      sourcemap: false,
+    },
+    {
+      file: pkg.module,
       format: "esm",
-      sourcemap: true
-    }
+      sourcemap: true,
+    },
   ],
   plugins: [
+    terser({
+      include: [/^.+\.min\.js$/],
+    }),
     peerDepsExternal(),
     resolve(),
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
     sass({
-      insert: true
+      insert: true,
     }),
     copy({
       targets: [
         {
           src: "src/variables.scss",
           dest: "build",
-          rename: "variables.scss"
+          rename: "variables.scss",
         },
         {
           src: "src/typography.scss",
           dest: "build",
-          rename: "typography.scss"
-        }
-      ]
-    })
-  ]
+          rename: "typography.scss",
+        },
+      ],
+    }),
+  ],
 };
