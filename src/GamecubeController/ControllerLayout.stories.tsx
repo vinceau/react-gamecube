300ea8f114d662480f8c10339534917850a6cdec
Vince Au <vinceau09@gmail.com>
2020-05-17 21:17:40 +1000

    Add clickable hidden analog stick to storybook

--- src/GamecubeController/ControllerLayout.stories.tsx
+++ src/GamecubeController/ControllerLayout.stories.tsx
@@ -14,53 +14,63 @@ export const HideAnalogSticks = (): JSX.Element => {
   return <ControllerLayout hideAnalogSticks={true} />;
 };
 
-export const ClickHandler = (): JSX.Element => {
+const toggleButton = (input: ButtonInput, oldState: Partial<ControllerInputState>): Partial<ControllerInputState> => {
+  const newState = {
+    ...oldState,
+  };
+  switch (input) {
+    case ButtonInput.A:
+      newState.a = !oldState.a;
+      break;
+    case ButtonInput.D_LEFT:
+      newState.dl = !oldState.dl;
+      break;
+    case ButtonInput.D_RIGHT:
+      newState.dr = !oldState.dr;
+      break;
+    case ButtonInput.D_DOWN:
+      newState.dd = !oldState.dd;
+      break;
+    case ButtonInput.D_UP:
+      newState.du = !oldState.du;
+      break;
+    case ButtonInput.Z:
+      newState.z = !oldState.z;
+      break;
+    case ButtonInput.R:
+      newState.r = !oldState.r;
+      break;
+    case ButtonInput.L:
+      newState.l = !oldState.l;
+      break;
+    case ButtonInput.B:
+      newState.b = !oldState.b;
+      break;
+    case ButtonInput.X:
+      newState.x = !oldState.x;
+      break;
+    case ButtonInput.Y:
+      newState.y = !oldState.y;
+      break;
+    case ButtonInput.START:
+      newState.start = !oldState.start;
+      break;
+  }
+  return newState;
+};
+
+export const Clickable = (): JSX.Element => {
   const [value, setValue] = React.useState<Partial<ControllerInputState>>({});
-  const onClick = (input: string): void => {
-    setValue((oldState) => {
-      const newState = {
-        ...oldState,
-      };
-      switch (input) {
-        case ButtonInput.A:
-          newState.a = !oldState.a;
-          break;
-        case ButtonInput.D_LEFT:
-          newState.dl = !oldState.dl;
-          break;
-        case ButtonInput.D_RIGHT:
-          newState.dr = !oldState.dr;
-          break;
-        case ButtonInput.D_DOWN:
-          newState.dd = !oldState.dd;
-          break;
-        case ButtonInput.D_UP:
-          newState.du = !oldState.du;
-          break;
-        case ButtonInput.Z:
-          newState.z = !oldState.z;
-          break;
-        case ButtonInput.R:
-          newState.r = !oldState.r;
-          break;
-        case ButtonInput.L:
-          newState.l = !oldState.l;
-          break;
-        case ButtonInput.B:
-          newState.b = !oldState.b;
-          break;
-        case ButtonInput.X:
-          newState.x = !oldState.x;
-          break;
-        case ButtonInput.Y:
-          newState.y = !oldState.y;
-          break;
-        case ButtonInput.START:
-          newState.start = !oldState.start;
-          break;
-      }
-      return newState;
-    });
+  const onClick = (input: ButtonInput): void => {
+    setValue((oldState) => toggleButton(input, oldState));
   };
   return <ControllerLayout value={value} onClick={onClick} />;
+};
+
+export const ClickableHideAnalogSticks = (): JSX.Element => {
+  const [value, setValue] = React.useState<Partial<ControllerInputState>>({});
+  const onClick = (input: ButtonInput): void => {
+    setValue((oldState) => toggleButton(input, oldState));
+  };
+  return <ControllerLayout hideAnalogSticks={true} value={value} onClick={onClick} />;
 };
