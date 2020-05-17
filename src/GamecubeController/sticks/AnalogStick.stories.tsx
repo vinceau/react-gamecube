b98e58b2908e27fa8c2998046341acc0b2b59588
Vince Au <vinceau09@gmail.com>
2020-05-17 16:00:15 +1000

    Add more analog stick positions

--- src/GamecubeController/sticks/AnalogStick.stories.tsx
+++ src/GamecubeController/sticks/AnalogStick.stories.tsx
@@ -13,3 +13,12 @@ export const FullLeft = (): JSX.Element => <AnalogStick x={1} y={0} color={color
 export const FullRight = (): JSX.Element => <AnalogStick x={-1} y={0} color={color} strokeColor={strokeColor} />;
 export const FullTop = (): JSX.Element => <AnalogStick x={0} y={1} color={color} strokeColor={strokeColor} />;
 export const FullBottom = (): JSX.Element => <AnalogStick x={0} y={-1} color={color} strokeColor={strokeColor} />;
+
+export const BottomLeft = (): JSX.Element => (
+  <AnalogStick x={-0.7625} y={-0.65} color={color} strokeColor={strokeColor} />
+);
+export const TopLeft = (): JSX.Element => <AnalogStick x={-0.6375} y={0.775} color={color} strokeColor={strokeColor} />;
+export const TopRight = (): JSX.Element => <AnalogStick x={0.625} y={0.7875} color={color} strokeColor={strokeColor} />;
+export const BottomRight = (): JSX.Element => (
+  <AnalogStick x={0.7125} y={-0.7} color={color} strokeColor={strokeColor} />
+);
--- src/GamecubeController/sticks/CStick.stories.tsx
+++ src/GamecubeController/sticks/CStick.stories.tsx
@@ -13,3 +13,8 @@ export const FullLeft = (): JSX.Element => <CStick x={1} y={0} color={color} str
 export const FullRight = (): JSX.Element => <CStick x={-1} y={0} color={color} strokeColor={strokeColor} />;
 export const FullTop = (): JSX.Element => <CStick x={0} y={1} color={color} strokeColor={strokeColor} />;
 export const FullBottom = (): JSX.Element => <CStick x={0} y={-1} color={color} strokeColor={strokeColor} />;
+
+export const BottomLeft = (): JSX.Element => <CStick x={-0.7625} y={-0.65} color={color} strokeColor={strokeColor} />;
+export const TopLeft = (): JSX.Element => <CStick x={-0.6375} y={0.775} color={color} strokeColor={strokeColor} />;
+export const TopRight = (): JSX.Element => <CStick x={0.625} y={0.7875} color={color} strokeColor={strokeColor} />;
+export const BottomRight = (): JSX.Element => <CStick x={0.7125} y={-0.7} color={color} strokeColor={strokeColor} />;
