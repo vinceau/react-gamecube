790e45c1fe7090449de9a3612ee129f65bdb7b05
Vince Au <vinceau09@gmail.com>
2020-05-27 00:01:16 +1000

    Fix LTrigger

--- src/buttons/LTrigger.tsx
+++ src/buttons/LTrigger.tsx
@@ -1,6 +1,8 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 import React from "react";
 
-import styled from "@emotion/styled";
+import { SVGUniqueID } from "react-svg-unique-id";
 
 export const LTrigger: React.FC<{
   pressed?: boolean;
@@ -10,64 +12,70 @@ export const LTrigger: React.FC<{
   backgroundColor?: string;
   onClick?: () => void;
 }> = (props) => {
-  const { hideButtonText, onClick, pressed } = props;
-  const value = pressed ? 1 : props.value ? props.value : 0;
+  const { hideButtonText, onClick, pressed, color, backgroundColor } = props;
+  const value = pressed ? 1 : props.value;
   const offset = `${(1 - value) * 100}%`;
-  const buttonColor = props.backgroundColor || "#8F8F8F";
-  const textColor = props.color || "#FFFFFF";
-  const Outer = styled.div`
-    box-sizing: content-box;
-    width: 15em;
-    ${onClick &&
-    `
-        cursor: pointer;
-        &:hover {
-            ${
-              pressed
-                ? "opacity: 0.85;"
-                : `
-            #path-1 {
-                fill: ${buttonColor};
+  const hoverStyles = css`
+    &:hover {
+      cursor: pointer;
+      ${pressed
+        ? "opacity: 0.85;"
+        : `
+            path {
+                fill: ${backgroundColor};
             }
             text {
-                fill: ${textColor};
+                fill: ${color};
             }
-            `
-            }
-        }
-    `}
-    text {
-      display: ${hideButtonText ? "none" : "inline"};
-      fill: ${pressed ? textColor : buttonColor};
-    }
-    svg {
-      overflow: visible;
-    }
-    #path-1 {
-      stroke: ${buttonColor};
+          `}
     }
   `;
   return (
-    <Outer onClick={onClick}>
-      <svg width="100%" viewBox="0 0 235 141" version="1.1" overflow="visible">
-        <linearGradient id="lprogress" x1="0.5" y1="1" x2="0.5" y2="0">
-          <stop offset="0%" stopOpacity="0" stopColor={buttonColor} />
-          <stop offset={offset} stopOpacity="0" stopColor={buttonColor} />
-          <stop offset={offset} stopOpacity="1" stopColor={buttonColor} />
-          <stop offset="100%" stopOpacity="1" stopColor={buttonColor} />
-        </linearGradient>
-        <g>
-          <path
-            id="path-1"
-            fill="url(#lprogress)"
-            strokeWidth="8"
-            d="M234.941486,37.9102532 C140.587902,69.5466833 51.1762154,107.627989 0.8999803,140.68745 C12.1209681,60.8935071 71.693988,0 143.5,0 C178.19432,0 210.03284,14.215631 234.941486,37.9102532 Z"
-          />
-          <text alignmentBaseline="middle" textAnchor="middle" x="50%" y="33%" fontSize="52">
-            L
-          </text>
-        </g>
-      </svg>
-    </Outer>
+    <div
+      onClick={onClick}
+      css={css`
+        box-sizing: content-box;
+        width: 15em;
+        ${onClick && hoverStyles}
+      `}
+    >
+      <SVGUniqueID>
+        <svg width="100%" viewBox="0 0 235 141" version="1.1" overflow="visible">
+          <linearGradient id="lprogress" x1="0.5" y1="1" x2="0.5" y2="0">
+            <stop offset="0%" stopOpacity="0" stopColor={backgroundColor} />
+            <stop offset={offset} stopOpacity="0" stopColor={backgroundColor} />
+            <stop offset={offset} stopOpacity="1" stopColor={backgroundColor} />
+            <stop offset="100%" stopOpacity="1" stopColor={backgroundColor} />
+          </linearGradient>
+          <g>
+            <path
+              fill="url(#lprogress)"
+              stroke={backgroundColor}
+              strokeWidth="8"
+              d="M234.941486,37.9102532 C140.587902,69.5466833 51.1762154,107.627989 0.8999803,140.68745 C12.1209681,60.8935071 71.693988,0 143.5,0 C178.19432,0 210.03284,14.215631 234.941486,37.9102532 Z"
+            />
+            <text
+              alignmentBaseline="middle"
+              textAnchor="middle"
+              x="50%"
+              y="33%"
+              fontSize="52"
+              fill={pressed ? color : backgroundColor}
+              css={css`
+                display: ${hideButtonText ? "none" : "inline"};
+              `}
+            >
+              L
+            </text>
+          </g>
+        </svg>
+      </SVGUniqueID>
+    </div>
   );
+};
+
+LTrigger.defaultProps = {
+  value: 0,
+  backgroundColor: "#8F8F8F",
+  color: "#FFFFFF",
 };
