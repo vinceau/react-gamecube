32ea9f9f6f40147f44ed9b4b5eefbdd6f0dd3991
Vince Au <vinceau09@gmail.com>
2020-05-27 21:07:21 +1000

    Reuse circle button styles

--- src/buttons/CircleButton.stories.tsx
+++ src/buttons/CircleButton.stories.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { AButton } from "./AButton";
+import { AButton, BButton, StartButton } from "./CircleButton";
 
 export default {
-  title: "AButton",
+  title: "CircleButton",
 };
 
-export const Default = (): JSX.Element => <AButton />;
+export const Default = (): JSX.Element => (
+  <div>
+    <AButton />
+    <BButton />
+    <StartButton />
+  </div>
+);
 export const AlternateColors = (): JSX.Element => <AButton pressed color="red" backgroundColor="pink" />;
 export const NoText = (): JSX.Element => <AButton hideButtonText={true} />;
 export const Pressed = (): JSX.Element => <AButton pressed />;
--- src/buttons/index.ts
+++ src/buttons/index.ts
@@ -1,8 +1,6 @@
-export { AButton } from "./AButton";
-export { BButton } from "./BButton";
+export { AButton, BButton, StartButton } from "./CircleButton";
 export { DpadDown, DpadLeft, DpadRight, DpadUp } from "./DpadButton";
 export { LTrigger, RTrigger } from "./TriggerButton";
-export { StartButton } from "./StartButton";
 export { XButton } from "./XButton";
 export { YButton } from "./YButton";
 export { ZButton } from "./ZButton";
